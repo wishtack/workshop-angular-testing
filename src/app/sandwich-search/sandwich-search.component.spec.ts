c9c3602771bf739bffc0c739539a83c6b6b4cec3
Younes JAAIDI <yjaaidi@gmail.com>
2019-09-25 14:41:01 +0200

    🚧 Search sandwiches. Comment-driven development.

--- src/app/sandwich-search/sandwich-search.component.spec.ts
+++ src/app/sandwich-search/sandwich-search.component.spec.ts
@@ -2,6 +2,9 @@ import { SandwichSearchComponent } from './sandwich-search.component';
 
 describe('SandwichSearchComponent', () => {
   xit('🚧 should search and display sandwiches', () => {
+    // @todo create and give a fake service to the component
+    // @todo search sandwiches with name 'Butter & Butter'
+    // @todo check that the sandwiches are set in some `sandwichList` property
     throw new Error('🚧 work in progress!');
   });
 
